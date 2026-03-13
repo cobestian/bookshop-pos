c6021efbb847a70a640b74cc716e8931f943a119
Cobestian Walker <cobestianwalker125@gamil.com>
2026-03-13 17:30:28 +0000

    fix netlify cors origin

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
